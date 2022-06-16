34641726a01568cde34bc76794266df96f3434df
lhaislla <lhaislla@gmail.com>
2022-06-16 00:52:37 -0300

    tag input

--- index.ts
+++ index.ts
@@ -1,37 +1,5 @@
-import { isCallOrNewExpression } from "typescript";
-
-console.log("TypeScript");
-function soma(a: number, b:number){
-    return a+b;
-}
-soma(2,3)
-
-interface IAnimal {
-    nome: string;
-    tipo: 'terrestre' | 'aquatico';
-   domestico: boolean;
-}
-interface ICanino extends IAnimal{
-    porte: 'pequeno'| 'medio' | 'grande';
-}
-interface IFelino extends IAnimal{
-    visãoNoturna: boolean;
-
-}
-type IDomestico = IFelino | ICanino;
-const animal: IDomestico ={
-   domestico: true,
-   nome: 'gato',
-   tipo:"terrestre",
-   porte: "pequeno",
-   visãoNoturna: true,
-    
-}
-
-
-/*const felino: IFelinos = {
-    nome:'Leão',
-    tipo: "terrestre",
-    visãoNoturna: true,
-    
-}*/
+const input= document.getElementById('input') as HTMLInputElement;
+input.addEventListener('input', (event) =>{
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value)
+})
